176e01af0fac5c86daf65542246ba9914a7c6406
WanderVictor <42519280+WanderVictor@users.noreply.github.com>
2018-11-07 22:06:35 -0200

    Add files via upload

---
+++ P4.c
@@ -0,0 +1,17 @@
+/*P4.c*/
+/* Wander Victor Verçosa Mares */
+/* 11811EAU010 */
+#include <stdio.h>
+int A(int, int);
+int main(){
+	int m=0, n=0;
+	scanf("%d %d", &m, &n);
+	printf("\n\n%d", A(m, n));
+	return 0;
+}
+int A(int m, int n){
+	if(m==0) return (n+1);
+	else if(m>0 && n==0) return A(m-1, 1);
+	else if(m>0 && n>0) return A(m-1, A(m, n-1));
+}
+
